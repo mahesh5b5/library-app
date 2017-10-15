b4f398c377d1553bccbcce32a9f80d0e18426623
mahesh5b5 <mk.talanki@gmail.com>
2017-10-15 17:52:26 +0530

    created routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesService } from './services/services.service';
+import { ProfileComponent } from './profile/profile.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule, routingComponents } from './app.routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    MenuComponent,
+    routingComponents,
+    FooterComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ServicesService],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
